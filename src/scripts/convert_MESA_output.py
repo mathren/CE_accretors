3cb142c6fa3778b28d7b8b61e23419f416c6f504
math <mathren90@gmail.com>
2022-06-15 14:05:12 -0400

    [skip ci] refactor

--- src/scripts/convert_MESA_output.py
+++ src/scripts/convert_MESA_output.py
@@ -3,25 +3,23 @@ Convert the tarball of the entire MESA output to binary.
 This allows showyourwork to cache the binary results on zenodo sandbox
 for faster reproduction
 """
-
 __author__ = "Mathieu Renzo"
 from MESAreader import getSrcCol
-import sys
 import os
 import paths
-import glob
 import tarfile
 
-tarball = tarfile.open(paths.data / "MESA_output.tar.gz", "r:gz")
-for member in tarball.getmembers():
-    # skip folders and only consider *.data files
-    if ".data" not in str(member): continue
-    # print(member.name)
+def clean_data_from_tarball(member):
     bin_fname = paths.data / str(member.name[:-4]+".npy")
     if not os.path.isfile(bin_fname):
         # file does not exist, extract
         tarball.extractall(path = paths.data, members=[member])
         src, col = getSrcCol(paths.data / member.name, True, True, bin_fname=bin_fname)
-    else:
-        print(str(bin_fname)+" found")
-tarball.close()
+
+if __name__ == "__main__":
+    tarball = tarfile.open(paths.data / "MESA_output.tar.gz", "r:gz")
+    # skip folders and only consider *.data files
+    data_files = [member for member in tarball.getmembers() if ".data" in str(member)]
+    for member in data_files:
+            clean_data_from_tarball(member)
+    tarball.close()
