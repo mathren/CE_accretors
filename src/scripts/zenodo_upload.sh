88eb88b0aed38a3a6f0db7151418fd9c19f2a1b5
mathren90 <mrenzo@flatironinstitute.org>
2022-08-31 13:40:07 -0400

    [skip ci] fix zenodo uploader script

--- src/scripts/zenodo_upload.sh
+++ src/scripts/zenodo_upload.sh
@@ -10,9 +10,4 @@ FILENAME="MESA_output.tar.gz"
 
 BUCKET=$(curl -H "Accept: application/json" -H "Authorization: Bearer $ZENODO_TOKEN" "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION" | jq --raw-output .links.bucket)
 
-echo "DEPOSITION" $DEPOSITION
-echo "FILENAME" $FILENAME
-echo "BUCKET"
-echo $BUCKET
-
 curl --progress-bar -o /dev/null --upload-file $FILEPATH/$FILENAME $BUCKET/$FILENAME?access_token=$ZENODO_TOKEN
