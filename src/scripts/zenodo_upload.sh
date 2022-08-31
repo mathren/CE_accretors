4e5e9ab2431bb673cfb823883e4d0a95b40be4e3
mathren90 <mrenzo@flatironinstitute.org>
2022-08-31 11:59:21 -0400

    [skip ci] simple script to upload on zenodo from workstation

---
+++ src/scripts/zenodo_upload.sh
@@ -0,0 +1,18 @@
+# assumes you have set ZENODO_TOKEN (see showyourwork docs)
+# DEPOSITION is the final number of the url of a "new version" on zenodo website
+# filename hardcoded, file path to be provided as argument
+
+set -xe
+
+DEPOSITION=$1
+FILEPATH=$2
+FILENAME="MESA_output.tar.gz"
+
+BUCKET=$(curl -H "Accept: application/json" -H "Authorization: Bearer $ZENODO_TOKEN" "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION" | jq --raw-output .links.bucket)
+
+echo "DEPOSITION" $DEPOSITION
+echo "FILENAME" $FILENAME
+echo "BUCKET"
+echo $BUCKET
+
+curl --progress-bar -o /dev/null --upload-file $FILEPATH/$FILENAME $BUCKET/$FILENAME?access_token=$ZENODO_TOKEN
